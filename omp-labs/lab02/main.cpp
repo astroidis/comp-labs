19fa2db9a65af1932e5d92f8e762f752c1d73a96
astroidis <69013280+astroidis@users.noreply.github.com>
2021-09-19 19:07:02 +0300

    lab02: test other options

--- omp-labs/lab02/main.cpp
+++ omp-labs/lab02/main.cpp
@@ -2,8 +2,10 @@
 #include <omp.h>
 #include <bitset>
 #include <limits>
+#include <format>
 
 using namespace std;
+
 
 namespace Reduction
 {
@@ -156,13 +158,245 @@ namespace Reduction
 	}
 }
 
+
+namespace Copyin
+{
+	int n;
+#pragma omp threadprivate(n)
+
+	int run()
+	{
+		n = 1;
+#pragma omp parallel copyin(n)  // all threads access the master thread's value
+		{
+			n = 2;
+			cout << "n = " << n << '\n';
+		}
+		cout << "Master n = " << n << '\n';
+
+		return 0;
+	}
+}
+
+
+namespace Dynamic
+{
+	int run()
+	{
+		cout << "OMP_DYNAMIC = " << omp_get_dynamic() << '\n';
+		
+		/* enables dynamic adjustment of the
+		 * number of threads available for the execution
+		 * to optimize resource usage
+		 * only 4 threads will be created later
+		 */
+		omp_set_dynamic(1);
+		cout << "OMP_DYNAMIC = " << omp_get_dynamic() << '\n';
+
+#pragma omp parallel num_threads(28)
+		{
+#pragma omp master
+			{
+				cout << "Parallel region, number of threads - " << omp_get_num_threads() << '\n';
+			}
+		}
+		return 0;
+	}
+}
+
+
+namespace Nested
+{
+	int run()
+	{
+		int n;
+		omp_set_nested(1);
+
+#pragma omp parallel private(n)
+		{
+			n = omp_get_thread_num();
+
+#pragma omp parallel
+			{
+				cout << format("Part 1, thread {} - {}\n", n, omp_get_thread_num());
+			}
+		}
+
+		omp_set_nested(0);
+
+#pragma omp parallel private(n)
+		{
+			n = omp_get_thread_num();
+
+#pragma omp parallel
+			{
+				cout << format("Part 2, thread {} - {}\n", n, omp_get_thread_num());
+			}
+		}
+	}
+
+	int nest3()
+	{
+		int l1, l2;
+		omp_set_nested(1);
+		omp_set_num_threads(2);
+
+#pragma omp parallel private(l1)
+		{
+			l1 = omp_get_thread_num();
+
+#pragma omp parallel private(l2)
+			{
+				l2 = omp_get_thread_num();
+
+#pragma omp parallel
+				{
+					cout << format("Level 1: {} Level 2: {} Level 3: {}\n", l1, l2, omp_get_thread_num());
+				}
+			}
+		}
+		return 0;
+	}
+}
+
+
+namespace InParallel
+{
+	void mode(void)
+	{
+		if (omp_in_parallel())
+		{
+			cout << "Parallel region\n";
+		}
+		else
+		{
+			cout << "Serial region\n";
+		}
+	}
+
+	int run()
+	{
+		mode();
+
+#pragma omp parallel
+		{
+#pragma omp master
+			{
+				mode();
+			}
+		}
+		return 0;
+	}
+}
+
+
+namespace Single
+{
+	int run()
+	{
+#pragma omp parallel
+		{
+			cout << "Message 1\n";
+
+			/* following code will be executed by only one of the threads
+			 * other threads will wait at implicit barrier at the end of this block
+			 * (so all "Message 1" will be printed before "Message 2")
+			 * to disable this syncronisation used `nowait`
+			 * (so output will be mixed up)
+			 */
+#pragma omp single nowait
+			{
+				cout << "Single thread\n";
+			}
+			cout << "Message 2\n";
+		}
+		return 0;
+	}
+}
+
+
+namespace Copyprivate
+{
+	int run()
+	{
+		int n;
+
+#pragma omp parallel private(n)
+		{
+			n = omp_get_thread_num();
+			cout << format("Start n = {}\n", n);
+
+#pragma omp single copyprivate(n)  // n will be shared among all threads
+			{
+				n = 100;
+			}
+
+			cout << format("End n = {}\n", n);
+		}
+		return 0;
+	}
+}
+
+
+namespace Master
+{
+	int run()
+	{
+		int n;
+
+#pragma omp parallel private(n)
+		{
+			n = 1;
+
+#pragma omp master
+			{
+				n = 2;
+			}
+
+			cout << "First n = " << n << '\n';
+
+#pragma omp barrier
+#pragma omp master
+			{
+				n = 3;
+			}
+			cout << "Second n = " << n << '\n';
+		}
+		return 0;
+	}
+}
+
+
+namespace MaxThreads
+{
+	int run()
+	{
+		cout << "Max number of threads " << omp_get_max_threads() << '\n';
+
+		return 0;
+	}
+}
+
+
 int main()
 {
+	//Reduction::plus();
+	//Reduction::minus();
+	//Reduction::multiply();
 	//Reduction::bit_and();
 	//Reduction::bit_or();
 	//Reduction::bit_xor();
 	//Reduction::bool_and();
-	Reduction::bool_or();
-
+	//Reduction::bool_or();
+	//Copyin::run();
+	//Dynamic::run();
+	//Nested::run();
+	Nested::nest3();
+	//InParallel::run();
+	//Single::run();
+	//Copyprivate::run();
+	//Master::run();
+	//MaxThreads::run();
+	
 	return 0;
 }
